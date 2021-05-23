27c24eb699ce0bb7b19c653bbc7e681253cbb1d3
lamccart15 <lamccart15@gmail.com>
2021-05-23 10:10:53 -0400

    DB trials

--- app.py
+++ app.py
@@ -1,29 +1,29 @@
 # Import libraries
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-from scrape_mars import scrape_info
+import scrape_mars
 
 # Create instance of Flask app
 app = Flask(__name__)
 
 # Establish Mongo connection
-uri="mongodb://localhost:27017/mars"
+uri="mongodb://localhost:27017/mars_db"
 mongo=PyMongo(app, uri=uri)
 
 # Create route that renders index.html template
 @app.route('/')
 def index(): 
-    post = db.posts.find_one({})
-    return render_template("index.html", post=post)
+    mars = mongo.db.mars.find_one({})
+    return render_template("index.html", mars=mars, space=post_data)
 
 @app.route('/scrape')
 def scrape(): 
 
     #Run scrape function
-    post = scrape()
+    post_data = scrape_mars.scrape()
 
     # Update Mongo database using update and upsert=True
-    mongo.db.post.update({}, post, upsert=True)
+    mongo.db.mars.update({}, post_data, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
--- scrape_mars.py
+++ scrape_mars.py
@@ -1,5 +1,5 @@
 from splinter import Browser
-from webriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup as bs
 import time
 
@@ -7,13 +7,16 @@ def scrape_info():
 
     # Setup Splinter
     executable_path={'executable_path': ChromeDriverManager().install()}
-    browser=Browser('chrome', **executable_path, headless=True)
+    browser=Browser('chrome', **executable_path, headless=False)
 
     # URL to scrape Mars news site
     mars_url= "https://redplanetscience.com/"
 
     # Connect to browser to collect html data
     browser.visit(mars_url)
+
+    time.sleep(1)
+
     html=browser.html
 
     # Create BeautifulSoup object 
@@ -23,12 +26,13 @@ def scrape_info():
     news_title = soup.find_all('div', class_='content_title')[0].text
     news_p = soup.find_all('div', class_='article_teaser_body')[0].text
 
-    browser.quit()
 
     # Save to MongoDB
     post = {
-        'title': news_title, 
-        'info': news_p
+        "title": news_title, 
+        "info": news_p
     }
+
+    browser.quit()
 
     return post
